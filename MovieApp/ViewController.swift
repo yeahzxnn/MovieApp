d431eab82355430ab2688e7414e11a95de0eba63
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-01-17 19:08:33 +0900

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by 신예진 on 2023/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
